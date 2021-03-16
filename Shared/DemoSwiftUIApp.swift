c991a91a2dbb16748d45d3d0fcb01b5bd8ec79a5
Mvestro <contact@ngarnier.fr>
2021-03-16 12:19:36 +0100

    first commit

---
+++ Shared/DemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIApp.swift
+//  Shared
+//
+//  Created by Mvestro on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
