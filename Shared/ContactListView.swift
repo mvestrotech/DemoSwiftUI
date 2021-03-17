4fbf3e2481a6ed6da7dacfcd8d0223daaadfd174
Mvestro <contact@ngarnier.fr>
2021-03-17 15:57:20 +0100

    cours3

---
+++ Shared/ContactListView.swift
@@ -0,0 +1,22 @@
+//
+//  ContactListView.swift
+//  DemoSwiftUI
+//
+//  Created by Mvestro on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContactListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactListView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContactListView()
+        }
+    }
+}
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,19 +10,27 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var name: String = ""
+    @State private var tracer = ContactTracer(contacts: [])
 
     var body: some View {
-        HStack {
-            TextField("Contact name", text: $name)
-            Button("Valider") {
-                let newContact = Contact(name: name, date: Date())
-            }
-        }.padding()
+        VStack {
+            HStack {
+                TextField("Contact name", text: $name)
+                Button("Valider") {
+                    let newContact = Contact(name: name, date: Date())
+                    tracer.add(contact: newContact)
+                    print(tracer.contacts)
+                }
+            }.padding()
+            Text(name)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
